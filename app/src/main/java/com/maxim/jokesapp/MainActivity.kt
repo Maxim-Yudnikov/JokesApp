f49aa1d5d730e3d9a8e68017fbfffcb53ee8fac8
Diryfort <dorhungmi2@gmail.com>
2023-10-26 12:53:05 +0300

    Lecture №16 completed

--- app/src/main/java/com/maxim/jokesapp/MainActivity.kt
+++ app/src/main/java/com/maxim/jokesapp/MainActivity.kt
@@ -3,37 +3,30 @@ package com.maxim.jokesapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.maxim.jokesapp.customView.FavoriteDataView
 import com.maxim.jokesapp.databinding.ActivityMainBinding
 import io.realm.Realm
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
         viewModel = (application as JokeApp).viewModel
 
-        binding.apply {
-            progressBar.visibility = View.INVISIBLE
-            getJokeButton.setOnClickListener {
-                getJokeButton.isEnabled = false
-                progressBar.visibility = View.VISIBLE
-                viewModel.getJoke()
-            }
+        val favoriteDataView = findViewById<FavoriteDataView>(R.id.jokeFavoriteDataView)
+        favoriteDataView.listenChanges { isChecked ->
+            viewModel.chooseFavorites(isChecked)
+        }
+        favoriteDataView.handleChangeButton {
+            viewModel.changeJokeStatus()
+        }
+        favoriteDataView.handleActionButton {
+            viewModel.getJoke()
+        }
 
-            favoriteButton.setOnClickListener {
-                viewModel.changeJokeStatus()
-            }
-
-            checkbox.setOnCheckedChangeListener { _, isChecked ->
-                viewModel.chooseFavorites(isChecked)
-            }
-
-            viewModel.observe(this@MainActivity) { state ->
-                state.show(progressBar, getJokeButton, jokeText, favoriteButton)
-            }
+        viewModel.observe(this) { state ->
+            favoriteDataView.show(state)
         }
     }
 }
